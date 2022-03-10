8b6dd272d47009cc5fcbeca9d77bdb10e182f7fa
georgios63 <georgemilitsopoulos63@gmail.com>
2022-03-10 14:41:57 +0100

    fetch data with axios

--- src/pages/Pokemons.jsx
+++ src/pages/Pokemons.jsx
@@ -1,12 +1,29 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const Pokemons = () => {
-  // const pokedex = async () => {
-  //   const result = await axios.get(
-  //     "https://pokeapi.co/api/v2/pokemon",
-  //     () => {}
-  //   );
+  const [pokemon, setPokemon] = useState();
+
+  const pokedex = async () => {
+    const response = await axios.get(
+      "https://pokeapi.co/api/v2/pokemon?limit=151"
+    );
+    setPokemon(response.data.results);
   };
-  return <p></p>;
+
+  useEffect(() => {
+    pokedex();
+  }, []);
+
+  return (
+    <div>
+      <h1>Pokedex!</h1>
+      {!pokemon ? (
+        <h1>"loading" </h1>
+      ) : (
+        pokemon.map((pokemon) => <li key={pokemon.name}>{pokemon.name}</li>)
+      )}
+    </div>
+  );
 };
